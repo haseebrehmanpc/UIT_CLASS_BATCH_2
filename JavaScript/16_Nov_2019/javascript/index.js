bf6cb17e4580acc8620aaa8d2a2e037684825ee5
haseebrehmanpc <haseebrehmanpc@gmail.com>
2019-11-16 16:04:04 +0500

    Class code

---
+++ JavaScript/16_Nov_2019/javascript/index.js
@@ -0,0 +1,17 @@
+// var text = 'Hello World.'
+
+// text = 'Hello Pakistan'
+
+// alert(text)
+// alert()
+
+var text1 = 'abc';
+
+var text2 = 456;
+
+alert(text1 - text2);
+
+
+// var name = "Haseeb";
+
+// alert("Hello, "+name);
